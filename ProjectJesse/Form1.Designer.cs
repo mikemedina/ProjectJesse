c25a09e169b178c8f194a307de6f15619752fd56
Mike Medina <mikemedina805@gmail.com>
2015-06-19 15:34:51 -0400

    Initial Commit
    A practice project involving C# and database management.

---
+++ ProjectJesse/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ProjectJesse
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(13, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(198, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(218, 9);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(55, 38);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 5;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(137, 40);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 78);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Name = "frmMain";
+            this.Text = "Import CSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ProjectJesse/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectJesse
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = ofd.FileName;
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            DataTable importedData = getData();
+
+            if (importedData == null) return;
+
+            saveToDatabase(importedData);
+
+            MessageBox.Show("Data imported successfully!");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private DataTable getData()
+        {
+            DataTable importedData = new DataTable();
+            try
+            {
+                using (StreamReader reader = new StreamReader(txtPath.Text))
+                {
+                    string firstLine = reader.ReadLine();
+                    if (string.IsNullOrEmpty(firstLine))
+                    {
+                        Console.WriteLine("No data");
+                        return null;
+                    }
+
+                    string[] columnHeaders = firstLine.Split(',');
+                    foreach (string columnHeader in columnHeaders)
+                    {
+                        importedData.Columns.Add(columnHeader);
+                    }
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (string.IsNullOrEmpty(line)) continue;
+
+                        string[] fields = line.Split(',');
+                        DataRow importedRow = importedData.NewRow();
+
+                        for (int i = 0; i < fields.Count(); i++)
+                        {
+                            importedRow[i] = fields[i];
+                        }
+                        importedData.Rows.Add(importedRow);
+                    }
+                    txtPath.Text = "";
+                    return importedData;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                return null;
+            }
+        }
+
+        private void saveToDatabase(DataTable importedData)
+        {
+            string connString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                foreach (DataRow importRow in importedData.Rows)
+                {
+                    decimal rate;
+                    decimal.TryParse(importRow["Rate"].ToString(), out rate);
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO ImportTable (StationID, ClientID, Rate, ShowID)" +
+                                                    "VALUES (@stationID, @clientID, @rate, @showID)", conn);
+                    cmd.Parameters.AddWithValue("@stationID", getStationID((string)importRow["StationName"]));
+                    cmd.Parameters.AddWithValue("@clientID", getClientID((string)importRow["ClientName"]));
+                    cmd.Parameters.AddWithValue("@rate", rate);//(decimal)importRow["Rate"]);
+                    cmd.Parameters.AddWithValue("@showID", getShowID((string)importRow["ShowTitleName"]));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private int getStationID(string stationName)
+        {
+            string connString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT Id FROM Station WHERE name = '" + stationName + "'", conn);
+
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+
+        private int getClientID(string clientName)
+        {
+            string connString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT Id FROM Client WHERE name = '" + clientName + "'", conn);
+
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+
+        private int getShowID(string showName)
+        {
+            string connString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT Id FROM ShowTitle WHERE name = '" + showName + "'", conn);
+
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+    }
+}
